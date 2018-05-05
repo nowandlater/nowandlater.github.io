c6fbbeec489e7135cd4b334c04969c2e890f5c89
nowandlater <38999171+nowandlater@users.noreply.github.com>
2018-05-05 04:03:27 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -34,7 +34,29 @@ $(function()
     
     video.onloadedmetadata = function(e){
       video.play();
-    }     
+    }    
+    
+    
+    
+    if(navigator.getUserMedia) { // Standard
+    navigator.getUserMedia({ video: true }, function(stream) {
+        video.src = stream;
+        video.play();
+    }, errBack);
+} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+    navigator.webkitGetUserMedia({ video: true }, function(stream){
+        video.src = window.webkitURL.createObjectURL(stream);
+        video.play();
+    }, errBack);
+} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+    navigator.mozGetUserMedia({ video: true }, function(stream){
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    }, errBack);
+}
+    
+    
+    
   },
     function(e){log(e.message);
   });  
